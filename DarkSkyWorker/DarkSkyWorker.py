31d527224f408421c99fdf95bdfaad625ed151cc
Martin Michalski <martmichals@gmail.com>
2019-11-28 12:53:07 -0600

    experimenting with darksky api

--- DarkSkyWorker/DarkSkyWorker.py
+++ DarkSkyWorker/DarkSkyWorker.py
@@ -2,11 +2,27 @@
 import config, logging
 
 # Imports for proper program functionality
-import json
+import os, json, requests
 
 # Global variables
 TAG = 'DarkSkyWorker - '
 API_KEY = config.DARK_SKY_API_KEY
 
-# testing 
+# Initializing the local file environment
+logging.info(TAG+'Initializing the local environment')
+for path in config.LOC_FOLS.values():
+    if not os.path.exists(path):
+        logging.info(TAG+'Creating '+path)
+        os.mkdir(path)
+    else:
+        logging.info(TAG+'Found '+path)
+
+# Downloading some files as a test
+url = 'https://api.darksky.net/forecast/'+API_KEY+'/42.3601,-71.0589,255657600?exclude=currently,flags'
+request = requests.get(url)
+
+with open(config.LOC_FOLS['surface']+'datapoint.json', 'wb') as file:
+    file.write(request.content)
+
+
 
--- DarkSkyWorker/config.py
+++ DarkSkyWorker/config.py
@@ -1,12 +1,12 @@
 import logging
 
 # Api Keys
-DARK_SKY_API_KEY = 'YOUR_KEY'
+DARK_SKY_API_KEY = ''
 
 # All local folders needed for proper program execution
 LOC_FOLS = {
     'data'    : 'data/',
-    'surface' : 'data/surface-data'
+    'surface' : 'data/surface-data/'
 }
 
 # Configuration for program logging
